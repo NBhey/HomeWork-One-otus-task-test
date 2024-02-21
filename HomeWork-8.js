ddafcae49ddd94e4b0a33111c88f5c57ab14f374
Oganes <og.marckaryan@yandex.ru>
2024-02-21 11:21:08 +0300

    Do homework-8

--- HomeWork-8.js
+++ HomeWork-8.js
@@ -2,9 +2,8 @@
 
 // Task 1: Запросите у пользователя дату в формате ДД.ММ.ГГГГ. Напишите программу, выводящую день недели во введённой дате. 
 
-let input = prompt('Введите дату в формате ДД.ММ.ГГГГ.')
-
-function dayOfWeek(){    
+function dayOfWeek(){   
+    let input = prompt('Введите дату в формате ДД.ММ.ГГГГ.') 
     let date = input.split('.');
     let [day,month,year] = date;
     const newDate = new Date(`${year}-${month}-${day}`);
@@ -12,3 +11,22 @@ function dayOfWeek(){
     let dayWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
     return dayWeek[dayOfWeek];
 }
+
+// Task 2: Написать программу, которая выводит в консоль количество минут, прошедшее с начала сегодняшнего дня.
+
+const today = new Date(); 
+const minute = today.getMinutes() + today.getHours()*60;
+console.log(minute)
+
+// Task 3: В двух переменных хранятся даты рождения двух пользователей в форматен ДД.ММ.ГГГГ. Написать программу, которая определяет более молодого пользователя
+
+let user1 = "20.03.1993";
+let user2 = "13.05.2003"; 
+
+function checkBirthday(somethingUser){
+    let [day,month,year] = somethingUser.split('.')
+    let date = new Date(`${year}-${month}-${day}`)
+    let timeStamp = date.getTime();
+    return timeStamp
+}
+checkBirthday(user1) > checkBirthday(user2) ? console.log('Старше user2') : console.log('Старше user1')
